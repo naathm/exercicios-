152332409be9b81c467f646b300b541b3f4b5a79
Nathaly <naathm>
2018-11-05 20:49:37 -0200

    Utilizando if.

---
+++ utilizandoif.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = "Dom Pedro I";
+$idade = "19";
+
+echo "Olá " . $nome . "! Tudo bem? <br>";
+
+
+if ($idade >= 21){
+    echo "Parabéns! Você já pode responder juridicamente pelos seus atos! Cuidado heim!";
+}else{
+    echo "Ufa! Você precisa esperar um pouco pra ser juridicamente responsabilizado pelos seus atos, mas estamos de olho, ok?";
+}
