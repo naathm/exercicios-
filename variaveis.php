eb3f6d61ad73ca45a346e669055355980d96ab55
Nathaly <naathm>
2018-11-05 20:02:38 -0200

    Utilizar variáveis para salvar dados pessoais.

---
+++ variaveis.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = "Francisco Joao";
+$sobrenome = "José Jesus";
+$endereco = "Rua do Socorro, 80";
+$idade = "25 anos";
+$telefone = "1234-5678";
+
+echo "Olá ";
+echo $nome;
+echo " ";
+echo $sobrenome;
+echo "! <br>";
+echo "Você ainda mora lá na ";
+echo $endereco;
+echo "? <br>";
+echo "Fiquei sabendo que você tem ";
+echo $idade;
+echo ". <br>";
+echo "Me confirma seu telefone, por favor? É o ";
+echo $telefone;
+echo "? <br>";
+
