9af02bd53c0c8a17d167619484b1ae51fff32837
Nathaly <naathm>
2018-11-11 13:29:25 -0200

    Listar o co conteúdo de uma array utilizando a estrutura DO-WHILE

---
+++ array.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$frutas = array(
+    "Mamão",
+    "Abacate",
+    "Pitaya",
+    "Tamarindo",
+    "Ameixa",
+    "Jambu",
+    "Açaí",
+    "Cupuaçu",
+    "Pequi",
+);
+
+$totaldeItensArray = count($frutas);
+echo "Total de itens array: " .$totaldeItensArray . "<br><br>";
+
+$contador = 0;
+    do{
+    echo $frutas[$contador] . "<br>";    
+    $contador++; 
+    }while($contador < $totaldeItensArray);
+
+
