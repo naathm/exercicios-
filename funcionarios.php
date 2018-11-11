78f98651e0b8d57263094470e3c4f87a347c7cc8
Nathaly <naathm>
2018-11-11 02:30:18 -0200

    Criar uma lista de funcionários.

---
+++ funcionarios.php
@@ -0,0 +1,34 @@
+<?php
+
+$funcionarios = array("Joao", "Pedro", "Maria", "Joaquim", "Davi", "Samuel", "Rute", "Zacarias", "Caetano", "Eduardo");
+
+echo $funcionarios[0];
+echo "<br>";
+
+echo $funcionarios[1];
+echo "<br>";
+
+echo $funcionarios[2];
+echo "<br>";
+
+echo $funcionarios[3];
+echo "<br>";
+
+echo $funcionarios[4];
+echo "<br>";
+
+echo $funcionarios[5];
+echo "<br>";
+
+echo $funcionarios[6];
+echo "<br>";
+
+echo $funcionarios[7];
+echo "<br>";
+
+echo $funcionarios[8];
+echo "<br>";
+
+echo $funcionarios[9];
+echo "<br>";
+
