38b1a5c2706da4c7057ce914076e5c1ab8d29ef2
Nathaly <naathm>
2018-11-11 23:12:14 -0200

    Imprimir os dados de um array utilizando somente echo current e echo next.

---
+++ capitais.php
@@ -0,0 +1,25 @@
+<?php
+
+$capitais = array(
+    "São Luis",
+    "Salvador",
+    "Terezina",
+    "Fortaleza",
+    "Natal",
+    "João Pessoa",
+    "Aracaju",
+    "Maceió",
+    "Belém",
+    "Manaus"
+);
+
+echo current($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
+echo next($capitais)."<br>";
